0698d9796b389ea481822aaa87b131351ed88bc8
Zhalgas Nurakhmetov <z.nurakhmetov@kazdream.kz>
2021-12-29 20:30:16 +0600

    add headers to pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   test: false,
-  apiUrl: 'http://127.0.0.1:8000',
-  webSocketUrl: 'ws://127.0.0.1:8000'
+  apiUrl: 'https://startupback.herokuapp.com',
+  webSocketUrl: 'ws://startupback.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   test: false,
-  apiUrl: 'http://127.0.0.1:8000',
-  webSocketUrl: 'ws://127.0.0.1:8000'
+  apiUrl: 'https://startupback.herokuapp.com',
+  webSocketUrl: 'ws://startupback.herokuapp.com'
 };
 
 /*
